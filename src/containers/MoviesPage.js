bf5f2d146fd2dbae1ec033902b7a6a296ffe7d8c
snoor_madih <snoor.madih@gmail.com>
2021-06-23 07:09:07 +0000

    Automatically backed up by Learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,11 +1,26 @@
+<<<<<<< HEAD
 import React from 'react';
 import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
 import MovieShow from "../components/MovieShow";
+=======
+// .src/containers/MoviesPage.js
+import React from 'react';
+import { Route } from 'react-router-dom';
+import MoviesList from '../components/MoviesList';
+import MovieShow from '../components/MovieShow';
+
+>>>>>>> 951ca9370aa0a5634fa6dd7f703351c076469898
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
     <Route exact path={match.url} render={() => <h3>Choose a movie from the list above</h3>}/>
     <Route path={`${match.url}/:movieId`} render={routerProps => <MovieShow {...routerProps} movies={movies} /> }/>
   </div>
+<<<<<<< HEAD
 )
+=======
+)
+
+export default MoviesPage
+>>>>>>> 951ca9370aa0a5634fa6dd7f703351c076469898
